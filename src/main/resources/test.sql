9f0b681043ddf5bd69260f19eff06c94aebdd684
serhiizem <serhiizem@gmail.com>
2019-01-11 18:34:19 +0200

    Basic test data

--- src/main/resources/test.sql
+++ src/main/resources/test.sql
@@ -1,4 +1,8 @@
-SELECT u.ID AS ID, u.USERNAME AS USERNAME, COUNT(o.ID) AS NUMBEROFORDERS
-FROM sys_user u LEFT JOIN orders o
-ON u.id = o.user_id
-GROUP BY u.ID, USERNAME
+INSERT INTO public.orders (id, name, user_id) VALUES (1, 'ORDER#1', 1);
+INSERT INTO public.orders (id, name, user_id) VALUES (2, 'ORDER#2', 1);
+INSERT INTO public.orders (id, name, user_id) VALUES (3, 'ORDER#3', 1);
+INSERT INTO public.orders (id, name, user_id) VALUES (4, 'ORDER#4', 2);
+INSERT INTO public.orders (id, name, user_id) VALUES (5, 'ORDER#5', 2);
+INSERT INTO public.sys_user (id, username) VALUES (1, 'User#1');
+INSERT INTO public.sys_user (id, username) VALUES (2, 'User#2');
+INSERT INTO public.sys_user (id, username) VALUES (3, 'User#3');
